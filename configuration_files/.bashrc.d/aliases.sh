1419597d4e3b4eef18fb29c022e76209a6d1c03c
Kirill Fedtsov <kirill.olegovich31@gmail.com>
2024-05-28 19:55:45 +0300

    starship: add new custom themes

--- configuration_files/.bashrc.d/aliases.sh
+++ configuration_files/.bashrc.d/aliases.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
+# Import aliases that are not for sharing in version control
+source $HOME/.bashrc.d/private_aliases.sh
+
 # alias prj="cd $HOME/Projects/; ls"
 # alias prj="cd $(find ~/Projects/ -maxdepth 2 -type d -not -name '.*' -and -not -name 'Projects' |  fzf); nvim .;"
 
-function _remote() {
- if [[ -z $1 ]]; then 
-    ssh cyrillus@213.159.208.102;
-  else
-    ssh $1@213.159.208.102;
- fi;
-}
-
-alias remote='clear; _remote'
 alias sysd="cd /etc/systemd/system/"
 alias open="gnome-open"
-alias devscience='clear; ssh ivan@10.242.107.104'
 alias wacomsetup='_wacomsetup' # see functions.sh
+
