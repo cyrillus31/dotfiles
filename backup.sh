0a81b3cb48adf7780628c9005083561cea4e6977
Kirill Fedtsov <kfedtsov@MacBook-Pro.local>
2025-07-22 11:49:10 +0300

    rename backup and add gitignore

---
+++ backup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+(mv "$HOME/.zshrc" "$HOME/.zshrc.bak" && echo ".zshrc.bak created") || true
+(mv "$HOME/.zshrc.d" "$HOME/.zshrc.d.bak" && echo ".zhsrc.d.bak created") || true
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mv $HOME/.zshrc $HOME/.zshrc.bak && echo ".zshrc.bak created" || true
-mv $HOME/.zshrc.d $HOME/.zshrc.d.bak && echo ".zhsrc.d.bak created" || true
-
-brew install stow
