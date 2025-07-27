4faf0e91bfaf8b40aaabd6b3838cb5f84ad05482
kfedtsov <kfedtsov@yandex-team.ru>
2025-07-27 23:21:43 +0300

    make everything depend on /Users/kfedtsov/arcadia

--- zsh/.zshrc.d/aliases.sh
+++ zsh/.zshrc.d/aliases.sh
@@ -3,6 +3,6 @@
 # Import aliases that are not for sharing in version control
 source $HOME/.bashrc.d/private.sh &>/dev/null || true
 
-alias bpy="cd $HOME/Projects/Arcadia/arcadia/taxi/backend-py3/"
-alias bgo="cd $HOME/Projects/Arcadia/arcadia/taxi/backend-go/"
+alias bpy="cd $ARC_DIR/taxi/backend-py3/"
+alias bgo="cd $ARC_DIR/taxi/backend-go/"
 
