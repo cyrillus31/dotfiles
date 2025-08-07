2029f164500e2e60f0f2290b83c92848a367df60
kfedtsov <kfedtsov@yandex-team.ru>
2025-08-07 15:49:06 +0300

    yandex(homebrew): add support for 4.4 mongo version if installed

--- shell_yandex/.shellrc.d/yandex/homebrew.sh
+++ shell_yandex/.shellrc.d/yandex/homebrew.sh
@@ -5,3 +5,6 @@
 # add yandex golang version to PATH
 YANDEX_GO_VERSION="/opt/homebrew/opt/go@1.23/bin"
 [ -d $YANDEX_GO_VERSION ] && append_path $YANDEX_GO_VERSION
+
+YANDEX_MONGO_VERSION="/opt/homebrew/opt/mongodb-community@4.4/bin"
+[ -d $YANDEX_MONGO_VERSION ] && append_path $YANDEX_MONGO_VERSION
