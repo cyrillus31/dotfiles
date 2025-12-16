c634af93e59f554aba2de4f276163ced64845c66
kfedtsov <kfedtsov@yandex-team.ru>
2025-12-16 10:22:20 +0300

    yandex: format alias command

--- shell_yandex/.shellrc.d/yandex/aliases.sh
+++ shell_yandex/.shellrc.d/yandex/aliases.sh
@@ -2,8 +2,16 @@
 
 
 alias logbroker="ya tool logbroker"
+# alias format="_format"
 alias format="ya tool tt format"
 alias update="ya project update gen"
 alias check="ya tool tt check"
 alias yatest="ya test"
 alias style="ya test --style"
+
+
+# DEPRECATED FOR NOW
+_format () {
+	echo "ya tool tt format $1 2>/dev/null"
+	ya tool tt format "$1" 2>/dev/null
+}
