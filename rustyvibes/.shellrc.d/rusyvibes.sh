0a546cee054446d935acb6658be48e89fa7fd962
kfedtsov <kfedtsov@yandex-team.ru>
2026-03-24 12:11:54 +0300

    rustyvibes: fix shellcheck linting issues and improve process detection
    - Quote all variables to prevent word splitting and globbing
    - Replace 'ps aux | grep' with 'pgrep -f' for better process detection
    - Use 'find' instead of 'ls' for safer directory listing
    - Add commented original lines as fallbacks for pgrep concerns

--- rustyvibes/.shellrc.d/rusyvibes.sh
+++ rustyvibes/.shellrc.d/rusyvibes.sh
@@ -4,24 +4,26 @@
 function rv {
   FOLDER=$HOME/rustyvibes_soundpacks/
   if [[ -z $1 ]]; then
-    kill $(ps aux | grep '[r]ustyvibes' | awk '{print $2}') &>/dev/null
-    echo $FOLDER
-    SOUNDPACK=$(ls $FOLDER | fzf --reverse --prompt 'Pick a soundpack> ' --header 'RUSTYVIBES Soundpacks: ')
+    # kill $(ps aux | grep '[r]ustyvibes' | awk '{print $2}') &>/dev/null
+    kill $(pgrep -f rustyvibes) &>/dev/null
+    echo "$FOLDER"
+    SOUNDPACK=$(find "$FOLDER" -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | fzf --reverse --prompt 'Pick a soundpack> ' --header 'RUSTYVIBES Soundpacks: ')
     if [[ -z $SOUNDPACK ]]; then
       echo "You need to pick a soundpack" >&2
       return 1
     fi
-    (rustyvibes $FOLDER/$SOUNDPACK &)
+    (rustyvibes "$FOLDER/$SOUNDPACK" &)
     echo "Rustyvibes is running!"
   else
     case $1 in
     -k|--kill)
-      process_id=$(ps aux | grep '[r]ustyvibes' | awk '{print $2}')
+      # process_id=$(ps aux | grep '[r]ustyvibes' | awk '{print $2}')
+      process_id=$(pgrep -f rustyvibes)
       if [[ -z $process_id ]]; then
         echo "Rustyvibes is not running"
         return 0
       fi
-      kill $process_id 2>/dev/null
+      kill "$process_id" 2>/dev/null
       echo "RUSTYVIBES process was terminated."
       return 0
       ;;
@@ -45,12 +47,12 @@ EOF
       ;;
     -l|--list)
       printf "++ Here are all available soundpacks ++\n\n"
-      ls -1 $FOLDER
+      ls -1 "$FOLDER"
       ;;
     -s|--soundpack)
-      if [[ -z $2 ]]; then echo "You didn't specify the soundpack."; exit 1; fi;
-      SP=$(find $FOLDER | grep $2 -m 1)
-      (rustyvibes $SP &)
+      if [[ -z "$2" ]]; then echo "You didn't specify the soundpack."; exit 1; fi;
+      SP=$(find "$FOLDER" | grep --max-count=1 "$2")
+      (rustyvibes "$SP" &)
       ;;
     *)
       echo "Invalid option $1. Use -h or --help for an overview of the commands." >&2
